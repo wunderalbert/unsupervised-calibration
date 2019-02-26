7dd7b2c06630b99b40234ebdb1e1e938475e8f64
Albert <albert@semmle.com>
2019-02-26 09:09:37 +0000

    change x axis to be proportional; use pointrange instead of ribbon

--- plotting.R
+++ plotting.R
@@ -51,10 +51,7 @@ plot_accuracy <- function(results, show_different_partitions = T){
     mutate(seed = paste0("Run ", seed),
            n_partitions = factor(n_partitions, 
                                  levels = n_partitions %>% unique %>% sort,
-                                 labels = n_partitions %>% unique %>% sort %>% paste0(" partitions")),
-           image_size_used = factor(image_size_used, 
-                                    levels = image_size_used %>% unique %>% sort,
-                                    labels = image_size_used %>% unique %>% sort %>% paste0(" pixels"))) %>%
+                                 labels = n_partitions %>% unique %>% sort %>% paste0(" partitions"))) %>%
     
     ggplot +
     
@@ -62,26 +59,32 @@ plot_accuracy <- function(results, show_different_partitions = T){
     
     stat_summary(aes(x = image_size_used,
                      y = recalibrated_accuracy,
-                     fill = "after recalibration",
+                     col = "after recalibration",
                      group = ""),
-                 geom = "ribbon", fun.data = mean_iq, alpha = .3) +
+                 geom = "line", fun.data = mean_iq) +
     stat_summary(aes(x = image_size_used,
                      y = recalibrated_accuracy,
                      col = "after recalibration",
+                     group = ""),
+                 geom = "pointrange", fun.data = mean_iq) +
+    
+    stat_summary(aes(x = image_size_used,
+                     y = raw_accuracy,
+                     col = "before recalibration",
                      group = ""),
                  geom = "line", fun.data = mean_iq) +
     stat_summary(aes(x = image_size_used,
                      y = raw_accuracy,
                      col = "before recalibration",
                      group = ""),
-                 geom = "line", fun.data = mean_iq) +
+                 geom = "pointrange", fun.data = mean_iq) +
     
     scale_color_manual("", 
                        values = c("blue", "red"),
                        aesthetics = c("fill", "colour")) +
     
     scale_y_continuous("accuracy", labels = percent) +
-    xlab("image size")
+    scale_x_continuous("image size", labels = function(x) x %>% paste0(" pixels"))
 }
 
 
@@ -141,13 +144,13 @@ plot_Brier_composition <- function(results, show_different_partitions = T){
     stat_summary(aes(x = image_size_used, y = recalibrated_Brier_score, col = "after recalibration"),
                  fun.data = mean_iq) +
     
-    stat_summary(aes(x = image_size_used, y = raw_Brier_score, col = "before recalibration", linetype = "Brier score"), 
+    stat_summary(aes(x = image_size_used, y = raw_Brier_score, col = "before recalibration", linetype = "Brier score", group = 1), 
                  geom = "line", fun.data = mean_iq) +
-    stat_summary(aes(x = image_size_used, y = recalibrated_Brier_score, col = "after recalibration", linetype = "Brier score"), 
+    stat_summary(aes(x = image_size_used, y = recalibrated_Brier_score, col = "after recalibration", linetype = "Brier score", group = 2), 
                  geom = "line", fun.data = mean_iq) +
-    stat_summary(aes(x = image_size_used, y = raw_Brier_calibration_score, col = "before recalibration", linetype = "Brier calibration score"), 
+    stat_summary(aes(x = image_size_used, y = raw_Brier_calibration_score, col = "before recalibration", linetype = "Brier calibration score", group = 3), 
                  geom = "line", fun.data = mean_iq) +
-    stat_summary(aes(x = image_size_used, y = recalibrated_Brier_calibration_score, col = "after recalibration", linetype = "Brier calibration score"),
+    stat_summary(aes(x = image_size_used, y = recalibrated_Brier_calibration_score, col = "after recalibration", linetype = "Brier calibration score", group = 4),
                  geom = "line", fun.data = mean_iq)+
     
     scale_linetype_manual("", values = c(`Brier score` = 1, `Brier calibration score` = 2), breaks = rev) +
